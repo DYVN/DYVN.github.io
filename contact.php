fd36337e447e63f7d557f0507f71d3b49c0fe934
DYVN <rmille90@gmail.com>
2018-06-27 23:44:15 -0400

    added PHP for email form. Test!

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset(_POST['submit'])) {
+    $name = $_POST['name'];
+    $date = $_POST['date'];
+    $mailfrom = $POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "thetrelifestyle@gmail.com";
+    $headers = "From: ".$mailfrom;
+    $txt = "You have a new request from " .$name."\n\n".$message;
+
+    mail($mailTo, $txt, $headers);
+    header("Location: index.html?mailsend");
+
+
+}
